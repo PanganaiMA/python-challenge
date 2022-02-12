66a2a4ee1a093a771500a8451bcf1b7e032ed6f1
PanganaiMA <99211072+PanganaiMA@users.noreply.github.com>
2022-02-12 22:32:05 +0200

    python-challenge

---
+++ PyBank/analysis/Financial Analysis.py
@@ -0,0 +1,12 @@
+Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Local Git Repository/python-challenge/PyBank/main2.py ======
+Financial Analysis
+--------------------------
+Total Months:  86
+Total: $38382578
+Total: $ -2288.2
+Greatest Increase in Profits: Feb-2012 $1926159
+Greatest Decrease in Profits: Sep-2013 $-2196167
+>>> 
---
+++ PyPoll/analysis/Election Results.py
@@ -0,0 +1,16 @@
+Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:\Local Git Repository\python-challenge\PyPoll\main.py =======
+Election Results
+--------------------------
+Total Votes: 3521001
+--------------------------
+Khan: 63.0% (2218231)
+Correy: 20.0% (704200)
+Li: 14.0% (492940)
+O'Tooley: 3.0% (105630)
+--------------------------
+Winner: Khan
+--------------------------
+>>> 
